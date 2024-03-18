9eb06c36c59c29b7845d29eba316532d74bccd83
Miguelit09 <miguel.p0908@gmail.com>
2024-03-18 17:16:34 -0500

    Se relacionó el proyecto relations con Postgresql

--- django/relations/relations/settings.py
+++ django/relations/relations/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'relations.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'relations',
+        'USER': 'postgres',
+        'PASSWORD': '123789654.',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
